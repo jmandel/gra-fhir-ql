d2abf9fda06b8d614860a66f792475251a5050f3
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2015-08-21 20:13:53 -0400

    With env port

--- serve.js
+++ serve.js
@@ -28,7 +28,7 @@ app.get('/:server', function (req, res) {
     });
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
